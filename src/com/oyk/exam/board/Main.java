d1ba065972fc4f6398e36ef3d25ccdc9d3bbe4f0
ohyoungkyu <come8908@gmail.com>
2022-07-06 22:36:51 +0900

    exit가 입력될 떄까지 명령어 계속 입력받기

--- src/com/oyk/exam/board/Main.java
+++ src/com/oyk/exam/board/Main.java
@@ -1,10 +1,23 @@
 package com.oyk.exam.board;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("== 게시판 v 0.1 ==");
         System.out.println("== 프로그램 시작 ==");
-        System.out.println("명령)");
+        while(true) {
+            System.out.print("명령)");
+            String cmd = sc.nextLine();
+
+            if( cmd.equals("exit")) {
+                break;
+            }
+            System.out.printf("입력된 명령어 : %s\n",cmd);
+        }
+
         System.out.println("== 프로그램 종료 ==");
     }
 }
