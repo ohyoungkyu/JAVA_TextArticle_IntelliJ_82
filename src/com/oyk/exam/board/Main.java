6a5896c2de7a76dbc9ca89e4a574ccbd24f5021d
ohyoungkyu <come8908@gmail.com>
2022-07-09 21:00:49 +0900

    C:/Program Files/Git/user/article/detail 명령으로 최근 게시물 보기

--- src/com/oyk/exam/board/Main.java
+++ src/com/oyk/exam/board/Main.java
@@ -10,6 +10,7 @@ public class Main {
         System.out.println("== 프로그램 시작 ==");
 
         int articlesLastId = 0;
+        Article lastArticle = null;
 
         while(true) {
 
@@ -30,8 +31,26 @@ public class Main {
                 articlesLastId = id;
 
                 Article article = new Article(id,title,body);
+                lastArticle = article;
 
                 System.out.printf("%d번 게시물이 입력되었습니다.\n", article.id);
+            }
+
+            else if(cmd.equals("/usr/article/detail")) {
+
+                if(lastArticle == null) {
+                    System.out.println("게시물이 존재하지 않습니다.");
+                    continue;
+                }
+
+                Article article = lastArticle;
+
+                System.out.println(" 게시물 상세내용 - ");
+
+                System.out.printf("번호 : %s\n", article.id);
+                System.out.printf("제목 : %s\n", article.title);
+                System.out.printf("내용 : %s\n", article.body);
+
             }
             else {
                 System.out.printf("입력된 명령어 : %s\n",cmd);
@@ -39,6 +58,7 @@ public class Main {
         }
 
         System.out.println("== 프로그램 종료 ==");
+        sc.close();
     }
 }
 
