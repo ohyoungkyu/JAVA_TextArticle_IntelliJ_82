a1b5ad8cddfd76b02a0db88e544643b981c0725e
ohyoungkyu <come8908@gmail.com>
2022-07-11 20:14:54 +0900

    Rq getIntParam 을 도입하여 소스코드 다이어트

--- src/com/oyk/exam/board/Rq.java
+++ src/com/oyk/exam/board/Rq.java
@@ -21,6 +21,26 @@ public class Rq {
         return params;
     }
 
+    public String getParam(String paramName, String defaultValue) {
+        if( params.containsKey(paramName) == false) {
+            return defaultValue;
+        }
+
+            return params.get(paramName);
+    }
+    public int getIntParam(String paramName, int defaultValue) {
+        if( params.containsKey(paramName) == false) {
+            return defaultValue;
+        }
+
+        try{
+            return Integer.parseInt(params.get(paramName));
+        }
+        catch (NumberFormatException e) {
+            return defaultValue;
+        }
+    }
+
     // 수정가능, if문 금지
     public String getUrlPath() {
         return urlPath;
--- src/com/oyk/exam/board/UsrArticleController.java
+++ src/com/oyk/exam/board/UsrArticleController.java
@@ -2,7 +2,6 @@ package com.oyk.exam.board;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class UsrArticleController {
     int articlesLastId;
@@ -44,20 +43,10 @@ public class UsrArticleController {
     }
 
     public void actionDetail(Rq rq) {
-        Map<String, String> params = rq.getParams();
+        int id = rq.getIntParam("id", 0);
 
-        if(params.containsKey("id") == false) {
-            System.out.println("id를 입력해주세요.");
-            return;
-        }
-
-        int id = 0;
-
-        try{
-            id = Integer.parseInt(params.get("id"));
-        }
-        catch (NumberFormatException e) {
-            System.out.println("id를 정수형태로 입력해주세요.");
+        if(id == 0) {
+            System.out.println("id를 올바르게 입력해주세요.");
             return;
         }
 
@@ -81,13 +70,11 @@ public class UsrArticleController {
         System.out.println("번호 / 제목");
         System.out.println("--------------------");
 
-        Map<String, String> params = rq.getParams();
+        String searchKeyword = rq.getParam("searchKeyword", "");
         //검색시작
         List<Article> filteredArticles = articles;
 
-        if( params.containsKey("searchKeyword")) {
-            String searchKeyword = params.get("searchKeyword");
-
+        if( searchKeyword.length() > 0) {
             filteredArticles = new ArrayList<>();
 
             for( Article article : articles) {
@@ -101,11 +88,8 @@ public class UsrArticleController {
 
         List<Article> sortedArticles = filteredArticles;
 
-        boolean orderByIdDesc = true;
-
-        if( params.containsKey("orderBy") && params.get("orderBy").equals("idAsc")) {
-            orderByIdDesc = false;
-        }
+        String orderBy = rq.getParam("orderBy", "idDesc");
+        boolean orderByIdDesc = orderBy.equals("idDesc");
 
         if( orderByIdDesc ) {
             sortedArticles = Util.reverseList(sortedArticles);
