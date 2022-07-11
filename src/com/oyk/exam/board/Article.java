bd68170073fd0be2bf18eb671b57e28bf533d504
ohyoungkyu <come8908@gmail.com>
2022-07-11 19:22:27 +0900

    클래스별 파일 분리

---
+++ src/com/oyk/exam/board/Article.java
@@ -0,0 +1,17 @@
+package com.oyk.exam.board;
+
+public class Article {
+    int id;
+    String title;
+    String body;
+
+    Article(int id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+    @Override
+    public String toString() {
+        return String.format("{id: %d, title: \"%s\", body: \"%s\"}", id, title, body);
+    }
+}
