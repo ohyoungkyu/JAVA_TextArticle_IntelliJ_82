eb5533c419f198213811433d1ec0a7bd774f67c2
ohyoungkyu <come8908@gmail.com>
2022-07-11 17:03:57 +0900

    입력된 id에 대해서 유효성 체크

--- src/com/oyk/exam/board/Main.java
+++ src/com/oyk/exam/board/Main.java
@@ -55,7 +55,20 @@ public class Main {
             }
 
             else if(rq.getUrlPath().equals("/usr/article/detail")) {
-                int id = Integer.parseInt(params.get("id"));
+                if(params.containsKey("id") == false) {
+                    System.out.println("id를 입력해주세요.");
+                    continue;
+                }
+
+                int id = 0;
+
+                try{
+                    id = Integer.parseInt(params.get("id"));
+                }
+                catch (NumberFormatException e) {
+                    System.out.println("id를 정수형태로 입력해주세요.");
+                    continue;
+                }
 
                 if(id > articles.size()) {
                     System.out.println("게시물이 존재하지 않습니다.");
