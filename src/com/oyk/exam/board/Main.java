ef5773feb76c10cc0b826d427adcc3948e051b04
ohyoungkyu <come8908@gmail.com>
2022-07-11 16:50:50 +0900

    복잡한 명령어를 처리하기 위해,Rq 도입

--- src/com/oyk/exam/board/Main.java
+++ src/com/oyk/exam/board/Main.java
@@ -1,8 +1,6 @@
 package com.oyk.exam.board;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static void makeTestData(List<Article> articles) {
@@ -31,11 +29,13 @@ public class Main {
             System.out.print("명령)");
             String cmd = sc.nextLine();
 
-            if( cmd.equals("exit")) {
+            Rq rq = new Rq(cmd);
+
+            if( rq.getUrlPath().equals("exit")) {
                 break;
             }
 
-            else if( cmd.equals("/usr/article/write")) {
+            else if( rq.getUrlPath().equals("/usr/article/write")) {
                 System.out.println("- 게시물 등록 -");
                 System.out.printf("제목 : ");
                 String title = sc.nextLine();
@@ -52,7 +52,7 @@ public class Main {
                 System.out.printf("%d번 게시물이 입력되었습니다.\n", article.id);
             }
 
-            else if(cmd.equals("/usr/article/detail")) {
+            else if(rq.getUrlPath().equals("/usr/article/detail")) {
 
                 if(articles.isEmpty()) {
                     System.out.println("게시물이 존재하지 않습니다.");
@@ -68,7 +68,7 @@ public class Main {
                 System.out.printf("내용 : %s\n", article.body);
 
             }
-            else if(cmd.equals("/usr/article/list")) {
+            else if(rq.getUrlPath().equals("/usr/article/list")) {
 
                 System.out.println(" 게시물 리스트 - ");
                 System.out.println("--------------------");
@@ -105,5 +105,57 @@ class Article {
     @Override
     public String toString() {
         return String.format("{id: %d, title: \"%s\", body: \"%s\"}", id, title, body);
+    }
+}
+
+class Rq {
+    private String url;
+    private String urlPath;
+    private Map<String, String> params;
+
+    // 필드추가가능
+
+    // 수정가능
+    Rq(String url) {
+        this.url = url;
+        urlPath = Util.getUrlPathFromUrl(this.url);
+        params = Util.getParamsFromUrl(this.url);
+    }
+
+    // 수정가능, if문 금지
+    public Map<String, String> getParams() {
+        return params;
+    }
+
+    // 수정가능, if문 금지
+    public String getUrlPath() {
+        return urlPath;
+    }
+}
+
+// 수정불가능
+class Util {
+    static Map<String, String> getParamsFromUrl(String url) {
+        Map<String, String> params = new HashMap<>();
+        String[] urlBits = url.split("\\?", 2);
+
+        if (urlBits.length == 1) {
+            return params;
+        }
+
+        String queryStr = urlBits[1];
+        for (String bit : queryStr.split("&")) {
+            String[] bits = bit.split("=", 2);
+            if (bits.length == 1) {
+                continue;
+            }
+            params.put(bits[0], bits[1]);
+        }
+
+        return params;
+    }
+
+    static String getUrlPathFromUrl(String url) {
+        return url.split("\\?", 2)[0];
     }
 }
